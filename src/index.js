d5582b27e95cd3cba61b29aaf31ca7746160e1ca
hirokey <hira8ouji@gmail.com>
2025-03-16 23:51:31 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const onClickAdd = () => {
+
+  const textEl = document.querySelector("#add-text");
+  const text = textEl.value;
+  textEl.value = "";
+
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  p.textContent = text;
+  const button =document.createElement("button");
+  button.textContent = "削除";
+
+  button.addEventListener("click", () => {
+    const deleteTarget = button.closest("li");
+    deleteTarget.remove();
+  });
+
+  div.append(p);
+  div.append(button);
+  li.append(div);
+
+  document.querySelector("#memo-list").append(li);
+
+}
+
+document.querySelector("#add-button").addEventListener("click", () => onClickAdd());
